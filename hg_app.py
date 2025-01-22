7c1d33889de1e6a67f97d46de804633b515a99f5
huiwenshi <Huiwenshi@users.noreply.huggingface.co>
2025-01-22 11:32:34 +0000

    Add wheel for custom_rasterizer for ZeroGPU and L40S (#9)
    - Add wheel for custom_rastermizer built against A100 and L40S (b45d71c9a17d7c20de2b58765af85a8f9bbe2c41)
    
    
    Co-authored-by: hysts <hysts@users.noreply.huggingface.co>

--- hg_app.py
+++ hg_app.py
@@ -9,8 +9,8 @@ if True:
     print("cd /home/user/app/hy3dgen/texgen/differentiable_renderer/ && bash compile_mesh_painter.sh")
     os.system("cd /home/user/app/hy3dgen/texgen/differentiable_renderer/ && bash compile_mesh_painter.sh")
     print('install custom')
-    subprocess.run(shlex.split("pip install . --no-build-isolation"), cwd="/home/user/app/hy3dgen/texgen/custom_rasterizer/", check=True)
-    
+    subprocess.run(shlex.split("pip install custom_rasterizer-0.1-cp310-cp310-linux_x86_64.whl"), check=True)
+
     IP = "0.0.0.0"
     PORT = 7860
 
