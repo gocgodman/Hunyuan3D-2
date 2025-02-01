4e58cc80b3bdc6b57b86b836765b3a34e3acfe69
gocgodman <gocgodman@nate.com>
2025-02-01 10:45:17 +0900

    Update __init__.py

--- hy3dgen/shapegen/__init__.py
+++ hy3dgen/shapegen/__init__.py
@@ -22,7 +22,7 @@
 # fine-tuning enabling code and other elements of the foregoing made publicly available
 # by Tencent in accordance with TENCENT HUNYUAN COMMUNITY LICENSE AGREEMENT.
 import torch
-device = torch.device("cpu")  # GPU 대신 CPU로 설정
+device = ("cpu")  # GPU 대신 CPU로 설정
 from .pipelines import Hunyuan3DDiTPipeline, Hunyuan3DDiTFlowMatchingPipeline
 from .postprocessors import FaceReducer, FloaterRemover, DegenerateFaceRemover
 from .preprocessors import ImageProcessorV2, IMAGE_PROCESSORS, DEFAULT_IMAGEPROCESSOR
